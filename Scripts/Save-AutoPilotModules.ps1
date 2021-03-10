1def57cfaeae3fc67a9dc11c8b10817446c6803f
David Segura <david@segura.org>
2021-03-10 14:49:28 -0600

    21.3.10.1

--- Scripts/Save-AutoPilotModules.ps1
+++ Scripts/Save-AutoPilotModules.ps1
@@ -1,2 +1,5 @@
 Save-Module -Name WindowsAutoPilotIntune -Path 'C:\Program Files\WindowsPowerShell\Modules' -Verbose
+if (-NOT (Test-Path 'C:\Program Files\WindowsPowerShell\Scripts')) {
+    New-Item -Path 'C:\Program Files\WindowsPowerShell\Scripts' -ItemType Directory -Force | Out-Null
+}
 Save-Script -Name Get-WindowsAutoPilotInfo -Path 'C:\Program Files\WindowsPowerShell\Scripts' -Verbose
