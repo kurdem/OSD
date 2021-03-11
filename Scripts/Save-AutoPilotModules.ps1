a504cb2b1f360e582ed1ee9863ff2e2f2563735b
David Segura <david@segura.org>
2021-03-10 19:28:20 -0600

    main

--- Scripts/Save-AutoPilotModules.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host -ForegroundColor White "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))"
-
-Save-Module -Name WindowsAutoPilotIntune -Path 'C:\Program Files\WindowsPowerShell\Modules' -Verbose
-if (-NOT (Test-Path 'C:\Program Files\WindowsPowerShell\Scripts')) {
-    New-Item -Path 'C:\Program Files\WindowsPowerShell\Scripts' -ItemType Directory -Force | Out-Null
-}
-Save-Script -Name Get-WindowsAutoPilotInfo -Path 'C:\Program Files\WindowsPowerShell\Scripts' -Verbose
--- Scripts/Test-OSDCloud4.ps1
+++
@@ -1,6 +0,0 @@
-$Global:StartOSDCloud = Get-Variable
-$Global:GitHubUser = $GitHubUser
-$Global:GitHubRepository = $Repository
-$Global:GitHubScript = $Script
-$Global:GitHubUrl = $Url
-
