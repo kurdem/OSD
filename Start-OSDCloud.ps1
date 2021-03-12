713dcac5df998d449c3a8110ad91578bcb9eff59
David Segura <david@segura.org>
2021-03-11 19:41:12 -0600

    21.3.11.5

--- Start-OSDCloud.ps1
+++ Start-OSDCloud.ps1
@@ -177,13 +177,13 @@ if (-NOT (Get-PSDrive -Name 'C')) {
 #===================================================================================================
 #   Scripts/Update-BIOS.ps1
 #===================================================================================================
-if ((Get-MyComputerManufacturer -Brief) -eq 'Dell') {
+<# if ((Get-MyComputerManufacturer -Brief) -eq 'Dell') {
     Write-Host -ForegroundColor DarkCyan    "================================================================="
     Write-Host -ForegroundColor White       "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) " -NoNewline
     Write-Host -ForegroundColor Green       "Scripts/Update-BIOS.ps1"
     Write-Warning "This step is not working as expected ..."
     Update-MyDellBIOS
-}
+} #>
 #===================================================================================================
 #   Scripts/Save-WindowsESD.ps1
 #===================================================================================================
