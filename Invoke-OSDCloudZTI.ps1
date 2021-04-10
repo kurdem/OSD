f52f3e1e1210efaa945678604cf0973050cc2d15
David Segura <david@segura.org>
2021-04-10 15:46:23 -0500

    Update Invoke-OSDCloudZTI.ps1

--- Invoke-OSDCloudZTI.ps1
+++ Invoke-OSDCloudZTI.ps1
@@ -1,24 +1,28 @@
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud Startup"
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud ..."
 Start-Sleep -Seconds 5
 
+#Change Display Resolution for Virtual Machine
 if ((Get-MyComputerModel) -match 'Virtual') {
     Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
     Set-DisRes 1600
 }
 
+#Make sure I have the latest OSD Content
 Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
 Install-Module OSD -Force
 Import-Module OSD -Force
-Start-Sleep -Seconds 5
 
-Write-Host  -ForegroundColor Cyan "Ejecting ISO"
-Start-Sleep -Seconds 5
+#TODO: Spend the time to write a function to do this and put it here
+#Write-Host  -ForegroundColor Cyan "Ejecting ISO"
+#Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise
+#Start OSDCloud ZTI the RIGHT way
+Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
 
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction"
-Start-Sleep -Seconds 5
+#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
 
+#Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
-wpeutil restart
+wpeutil reboot
