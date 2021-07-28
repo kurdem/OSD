124505bedb12010b0d5141861077f5ee809b3808
David Segura <david@segura.org>
2021-07-28 13:57:20 -0500

    21.7.28.1

--- Tasks/2e25ddcb-ce05-438b-944c-c581268cfb7f.ps1
+++ Tasks/2e25ddcb-ce05-438b-944c-c581268cfb7f.ps1
@@ -1,21 +1,6 @@
 Write-Host  -ForegroundColor Cyan 'Start-OSDCloud with Params'
 Start-Sleep -Seconds 5
 #=======================================================================
-#   Set-DisRes
-#=======================================================================
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor DarkCyan 'Setting Virtual Machine Display Resolution to 1600x'
-    Write-Host  -ForegroundColor DarkCyan 'Set-DisRes 1600'
-    Set-DisRes 1600
-}
-#=======================================================================
-#   Update the OSD Module
-#=======================================================================
-Write-Host  -ForegroundColor DarkCyan 'Install-Module OSD -Force'
-Install-Module OSD -Force
-Write-Host  -ForegroundColor Cyan 'Import-Module OSD -Force'
-Import-Module OSD -Force
-#=======================================================================
 #   Params
 #=======================================================================
 $Params = @{
@@ -29,7 +14,5 @@ Start-OSDCloudGUI @Params
 #=======================================================================
 #   Restart-Computer
 #=======================================================================
-Write-Host  -ForegroundColor Cyan 'WinPE will reboot in 15 seconds'
-Start-Sleep -Seconds 15
-Write-Host  -ForegroundColor DarkCyan 'Restart-Computer'
+Start-Sleep -Seconds 10
 Restart-Computer
