d6721a3671a1b676174d3ed876f68ba20a52c43a
David Segura <david@segura.org>
2021-07-28 13:51:27 -0500

    21.7.28.1

--- Tasks/2e25ddcb-ce05-438b-944c-c581268cfb7f.ps1
+++ Tasks/2e25ddcb-ce05-438b-944c-c581268cfb7f.ps1
@@ -1,8 +1,5 @@
-#This demo shows how to use OSDCloud rebooting to Audit Mode and running OOBEDeploy
-#You can modify your OSDCloud WinPE by running the following command
-#Edit-OSDCloud.winpe -WebPSScript 'https://raw.githubusercontent.com/OSDeploy/OSDCloud/main/Samples/AuditModeOOBEDeploy.ps1'
-Write-Host  -ForegroundColor Cyan 'Demo OSDCloud Audit Mode OOBEDeploy'
-Start-Sleep -Seconds 10
+Write-Host  -ForegroundColor Cyan 'Start-OSDCloud with Params'
+Start-Sleep -Seconds 5
 #=======================================================================
 #   Set-DisRes
 #=======================================================================
@@ -12,71 +9,23 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 #=======================================================================
-#   OSD Module
+#   Update the OSD Module
 #=======================================================================
 Write-Host  -ForegroundColor DarkCyan 'Install-Module OSD -Force'
 Install-Module OSD -Force
 Write-Host  -ForegroundColor Cyan 'Import-Module OSD -Force'
 Import-Module OSD -Force
 #=======================================================================
+#   Params
+#=======================================================================
+$Params = @{
+    OSBuild = "21H1"
+    OSLanguage = "en-us"
+}
+#=======================================================================
 #   Start-OSDCloudGUI
 #=======================================================================
-Start-OSDCloudGUI
-#=======================================================================
-#   Unattend.xml
-#=======================================================================
-$AuditUnattendXml = @'
-<?xml version="1.0" encoding="utf-8"?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend">
-    <settings pass="oobeSystem">
-        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <Reseal>
-                <Mode>Audit</Mode>
-            </Reseal>
-        </component>
-    </settings>
-    <settings pass="auditUser">
-        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <RunSynchronous>
-            
-            <RunSynchronousCommand wcm:action="add">
-            <Order>1</Order>
-            <Description>Setting PowerShell ExecutionPolicy</Description>
-            <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy RemoteSigned -Force"</Path>
-            </RunSynchronousCommand>
-
-            <RunSynchronousCommand wcm:action="add">
-            <Order>2</Order>
-            <Description>Update OSD Module</Description>
-            <Path>PowerShell -Command "Install-Module OSD -Force"</Path>
-            </RunSynchronousCommand>
-
-            <RunSynchronousCommand wcm:action="add">
-            <Order>3</Order>
-            <Description>OOBEDeploy</Description>
-            <Path>PowerShell -Command "Start-OOBEDeploy -AddRSAT -AddNetFX3 -UpdateDrivers -UpdateWindows"</Path>
-            </RunSynchronousCommand>
-
-            </RunSynchronous>
-        </component>
-    </settings>
-</unattend>
-'@
-#=======================================================================
-#   Audit Mode
-#=======================================================================
-$PantherUnattendPath = 'C:\Windows\Panther\Unattend'
-if (-NOT (Test-Path $PantherUnattendPath)) {
-    New-Item -Path $PantherUnattendPath -ItemType Directory -Force | Out-Null
-}
-
-$AuditUnattendPath = Join-Path $PantherUnattendPath 'Unattend.xml'
-
-Write-Host -ForegroundColor Cyan "Set Unattend.xml at $AuditUnattendPath"
-$AuditUnattendXml | Out-File -FilePath $AuditUnattendPath -Encoding utf8
-
-Write-Host -ForegroundColor Cyan 'Use-WindowsUnattend'
-Use-WindowsUnattend -Path 'C:\' -UnattendPath $AuditUnattendPath -Verbose
+Start-OSDCloudGUI @Params
 #=======================================================================
 #   Restart-Computer
 #=======================================================================
